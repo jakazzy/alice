156b3b5da372321cccbc22568ef0face769c82b6
jakazzy <jakazzy@gmail.com>
2020-06-21 04:07:51 +0000

    config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-import "dotenv/config";
+import 'dotenv/config'
 
 export default {
-  FB_PAGE_TOKEN: "",
-  FB_VERIFY_TOKEN: "",
-  FB_APP_SECRET: "",
-  SERVER_URL: "",
-  GOOGLE_PROJECT_ID: "",
-  DF_LANGUAGE_CODE: "",
-  GOOGLE_CLIENT_EMAIL: "",
-  GOOGLE_PRIVATE_KEY: "",
-};
+    fbPgeToken: process.env.FB_PAGE_TOKEN,
+    fbVerfyToken: process.env.VERIFY_TOKEN,
+    fbAppSecret: process.env.FB_APP_SECRET,
+    serverUrl: process.env.SERVER_URL,
+    gglePrjctId: process.env.GOOGLE_PROJECT_ID,
+    dfLangCode: process.env.DF_LANGUAGE_CODE,
+    ggleClientId: process.env.GOOGLE_CLIENT_EMAIL,
+    gglePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+}
